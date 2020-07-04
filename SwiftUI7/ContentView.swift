66b85bce59efd0d284c0936215d7da6ec3059b4c
Rohit Saini <sketchlearn7@gmail.com>
2020-07-04 18:21:56 +0530

    Code Updated

--- SwiftUI7/ContentView.swift
+++ SwiftUI7/ContentView.swift
@@ -9,13 +9,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, World!")
+        Item()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct Item:View {
+    @State var data = ["1","2","3","4","5","6","7","8","9","10"]
+    var body: some View{
+        NavigationView{
+            List{
+                ForEach(data,id:\.self){ i in
+                    Text(i)
+                }
+                .onDelete(perform: {(index) in
+                    self.data.remove(atOffsets:index)
+                })
+                    .onMove(perform: {(index,destination) in
+                        self.data.move(fromOffsets: index,toOffset:destination)
+                    })
+            }
+        .navigationBarTitle("ListView")
+            .navigationBarItems(trailing:EditButton())
+        }
     }
 }
